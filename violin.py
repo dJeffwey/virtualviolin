ec52344eba6a26df98823cc9e85f1c40bdf67f78
dJeffwey <153790629+dJeffwey@users.noreply.github.com>
2024-09-05 19:54:57 -0700

    Update violin.py

--- violin.py
+++ violin.py
@@ -18,6 +18,14 @@ info = pygame.display.Info()
 window = pygame.display.set_mode((info.current_w, info.current_h), pygame.FULLSCREEN)
 pygame.display.set_caption("Virtual Violin")
 
+# Key to sound mapping
+key_map = {
+    pygame.K_1: 'e',  # 1 key to E sound
+    pygame.K_q: 'a',  # Q key to A sound
+    pygame.K_a: 'd',  # A key to D sound
+    pygame.K_z: 'g'   # Z key to G sound
+}
+
 # Function to play sound
 def play_sound(sound_file):
     pygame.mixer.music.load(sound_file)
@@ -36,15 +44,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            key = pygame.key.name(event.key)
-            if key in sounds:
-                if key not in playing_sounds:
-                    play_sound(sounds[key])
-                    playing_sounds[key] = sounds[key]
+            if event.key in key_map:
+                key = key_map[event.key]
+                if key in sounds:
+                    if key not in playing_sounds:
+                        play_sound(sounds[key])
+                        playing_sounds[key] = sounds[key]
         elif event.type == pygame.KEYUP:
-            key = pygame.key.name(event.key)
-            if key in playing_sounds:
-                stop_sound()
-                del playing_sounds[key]
+            if event.key in key_map:
+                key = key_map[event.key]
+                if key in playing_sounds:
+                    stop_sound()
+                    del playing_sounds[key]
 
 pygame.quit()
